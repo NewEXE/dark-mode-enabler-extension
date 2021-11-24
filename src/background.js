f51d01180f59eb01834bf3c8a6e9ffaddb7a40ba
NewEXE <v.voloshyn96@gmail.com>
2021-11-24 18:53:10 +0200

    Minor refactoring

--- src/background.js
+++ src/background.js
@@ -1,11 +1,11 @@
 function switchDarkMode() {
-  const tag = 'documentElement'; // <html> tag
+  const tag = document.documentElement; // <html> tag
   const cssFilter = 'invert(90%)';
 
-  if (document[tag].style.filter === cssFilter) {
-    document[tag].style.filter = '';
+  if (tag.style.filter === cssFilter) {
+    tag.style.filter = '';
   } else {
-    document[tag].style.filter = cssFilter;
+    tag.style.filter = cssFilter;
   }
 }
 
